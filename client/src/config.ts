010706bf75e02d6873b7654655a239bd6dcaef00
tranquocnha <tranquocnha1808000@gmail.com>
2022-12-03 23:42:49 +0700

    update submit

---
+++ client/src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = `https://yqaxpkrlp8.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-4jl6m2yxgl6fsk7z.us.auth0.com',
+  clientId: 'a6a0pfQAwlTVcBslutT99VlrzaO3ZURS',
+  callbackUrl: 'http://localhost:3000/callback'
+}
